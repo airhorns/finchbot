3a8bcea7f82f2244de2bfc7eb98080075e32338a
Harry Brundage <harry@skylightlabs.ca>
2010-09-16 10:39:44 -0400

    Added proper timeout count and finalized swich to string IDs

--- lib/darwin/game_player.rb
+++ lib/darwin/game_player.rb
@@ -3,12 +3,12 @@ require 'system_timer'
 module Finch
   class GamePlayer
     @queue = :games
-    @max_turn_length = 600
+    @max_turn_count = 600
+    @max_turn_length = 5000
     def self.perform(bot1, bot2, map, id)
-      puts Finch::Chromosome.find(id)
-      return true
-      SystemTimer.timeout_after((@max_turn_length+15).seconds) do
-        cmd = "java -jar tools/PlayGame.jar #{map} 5000 #{@max_turn_length} logs/log.txt \"#{bot1}\" \"#{bot2}\""
+      chromosome = Finch::Chromosome.find(id)
+      SystemTimer.timeout_after(((@max_turn_count*@max_turn_length)+15).seconds) do
+        cmd = "java -jar tools/PlayGame.jar #{map} #{@max_turn_length} #{@max_turn_count} logs/log.txt \"#{bot1}\" \"#{bot2}\""
         puts "Running: #{cmd}"
 
         pid, stdin, stdout, stderr = Open4::popen4 cmd
@@ -17,14 +17,13 @@ module Finch
         output = stderr.read
         score = self.parse_output(output)
         puts "score      : #{ score }"
-        s = Finch::Chromosome.find(id["$oid"])
-        s.scores.create!(:rating => score, :player1 => bot1, :player2 => bot2, :map => map)
+        chromosome.scores.create!(:rating => score, :player1 => bot1, :player2 => bot2, :map => map)
       end
     end
 
     def self.parse_output(string)
       if string.match("WARNING: player 2 timed out")
-        return -1 * (@max_turn_length + 1)
+        return -1 * (@max_turn_count + 1)
       else
         turns = string.scan(/Turn \d{1,3}/).length
         if string.match("Player 2 Wins!")
@@ -32,7 +31,7 @@ module Finch
         else
           winner = -1
         end
-        return (@max_turn_length - turns) * winner
+        return (@max_turn_count - turns) * winner
       end
     end
   end
